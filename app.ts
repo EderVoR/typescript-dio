605dadd87f4127f8b7af2d57c7f93a950f63eb55
Eder Vieira <eder.edy@gmail.com>
2022-08-14 22:40:25 -0400

    Trabalhando com Typescript

---
+++ app.ts
@@ -0,0 +1,13 @@
+let button = document.getElementById("button");
+let input1 = document.getElementById("input1") as HTMLInputElement;
+let input2 = document.getElementById("input2") as HTMLInputElement;
+
+function somaNumeros(numero1: number, numero2: number){
+    return numero1 + numero2;
+}
+
+if(button){
+    button.addEventListener('click', () =>{
+        somaNumeros(Number(input1.value), Number(input2.value));
+    });
+}
